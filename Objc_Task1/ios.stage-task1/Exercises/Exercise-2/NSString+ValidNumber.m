3cbd12f0ce54a93eca79bd664f85c968e0c29cbb
Maksim Marushchak <maksim.marushchak@leovegas.com>
2021-05-08 07:57:20 +0200

    add:exercise-2

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
@@ -3,7 +3,17 @@
 @implementation NSString (ValidNumber)
 
 - (BOOL)isValidNumber {
-    return false;
+    
+    if (self.length >= 0 || self.length <= 100) {
+
+         NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc] init];
+        
+         numberFormatter.numberStyle = NSNumberFormatterDecimalStyle;
+         NSNumber *number = [numberFormatter numberFromString:self];
+
+         return number != 0 ? true : false;
+        
+    } return false;
 }
 
 @end
