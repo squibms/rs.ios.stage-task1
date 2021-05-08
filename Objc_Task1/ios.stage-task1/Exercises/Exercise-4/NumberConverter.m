65fce4458c945b94df99a7e5c313904e01b68c48
Maksim Marushchak <maksim.marushchak@leovegas.com>
2021-05-08 10:59:07 +0200

    add:exercise-4

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
@@ -3,7 +3,18 @@
 @implementation NumberConverter
 
 - (NSArray *)numberConverter:(NSNumber *)number {
-    return nil;
+    
+    int unsignedIntValue = abs([number intValue]);
+    NSMutableArray *convertedArray = [NSMutableArray new];
+    
+    while (unsignedIntValue !=0) {
+        
+    [convertedArray addObject:[NSString stringWithFormat:@"%d", unsignedIntValue % 10]];
+                 unsignedIntValue /= 10;
+        
+    }
+    return convertedArray;
+    
 }
 
 @end
