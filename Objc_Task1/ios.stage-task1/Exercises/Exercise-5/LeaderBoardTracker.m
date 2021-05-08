96c9f5a18798e9a350f55b19cd3d24d1f5e6d711
Maksim Marushchak <maksim.marushchak@leovegas.com>
2021-05-08 18:28:06 +0200

    add:exercise-5

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-5/LeaderBoardTracker.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-5/LeaderBoardTracker.m
@@ -10,7 +10,26 @@
 
 - (NSArray <NSNumber *> *)trackLeaderBoard:(NSArray <NSNumber *> *)rankedArray
                                playerArray:(NSArray <NSNumber *> *)playerArray {
-    return @[];
+    
+    NSMutableArray *finalRanks = NSMutableArray.array;
+    NSUInteger counter = [rankedArray count];
+    int count = 0;
+
+        for (int i = 0; i < [playerArray count]; i++) {
+            count = 1;
+            for (int j = 0; j < counter; j++) {
+                if ([playerArray[i] intValue] >= [rankedArray[j] intValue]) {
+                    break;
+                } else {
+                    if (j+1 == counter || rankedArray[j+1] != rankedArray[j]) {
+                    count++;
+                    }
+                }
+            }
+            [finalRanks addObject : [NSNumber numberWithInt : count]];
+        }
+
+        return finalRanks;
 }
 
 @end
