93e6fce1389ecff98076c2ba3e43f356d556da0d
Maksim Marushchak <maksim.marushchak@leovegas.com>
2021-05-08 08:36:39 +0200

    add:exercise-3

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
@@ -3,7 +3,29 @@
 @implementation NSArray (MinRotated)
 
 - (NSNumber *)minRotated {
-    return nil;
+    
+    long arrayLength = [self count];
+
+     if (arrayLength == 0) {
+         return 0;
+     }
+
+     if ([self[0] intValue] <= [self[arrayLength - 1] intValue]) {
+         return self[0];
+     }
+
+     if (arrayLength == 2) {
+         return [self[0] intValue] < [self[1] intValue] ? self[0] : self[1];
+     }
+
+     long mid = arrayLength % 2 == 0 ? arrayLength / 2 : (arrayLength + 1) / 2;
+
+     if ([self[0] intValue] < [self[mid - 1] intValue]) {
+         
+         return [[self subarrayWithRange:NSMakeRange(mid, arrayLength - mid)] minRotated];
+     } else {
+         return [[self subarrayWithRange:NSMakeRange(0, mid)] minRotated];
+     }
 }
 
 @end
